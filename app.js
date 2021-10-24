45ad9268c45b74c9f30ac12a0bd5f68c00be0395
William Prudencio <36481731+wprudencio97@users.noreply.github.com>
2021-10-24 14:31:23 -0400

    Updated server port.

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.post("/", (req, res) => {
 		});
 });
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}.`));
